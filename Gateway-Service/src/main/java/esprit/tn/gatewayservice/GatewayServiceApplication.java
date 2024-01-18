58290cec49e621736ddb339876505df9cac201dd
Hanen-Azzouz <hanen-azzouz@hotmail.com>
2024-01-18 22:56:19 +0100

    First Commit MS-Edge

---
+++ Gateway-Service/src/main/java/esprit/tn/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,27 @@
+package esprit.tn.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+
+
+    // Dynamic Routes Configuration
+    @Bean
+    DiscoveryClientRouteDefinitionLocator definitionLocator(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties properties) {
+
+
+        return new DiscoveryClientRouteDefinitionLocator(rdc, properties);
+    }
+}
