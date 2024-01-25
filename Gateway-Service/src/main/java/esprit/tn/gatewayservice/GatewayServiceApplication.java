9dd6c5dbfc64f6db158f3a46c5d51018a60d612b
amine-oueslati-esprit <amine.oueslati@esprit.tn>
2024-01-25 11:07:05 +0100

    after making it docker files & compose work

--- Gateway-Service/src/main/java/esprit/tn/gatewayservice/GatewayServiceApplication.java
+++ Gateway-Service/src/main/java/esprit/tn/gatewayservice/GatewayServiceApplication.java
@@ -1,5 +1,4 @@
 package esprit.tn.gatewayservice;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -7,21 +6,15 @@ import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
 import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.context.annotation.Bean;
-
 @EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(GatewayServiceApplication.class, args);
     }
-
-
     // Dynamic Routes Configuration
     @Bean
     DiscoveryClientRouteDefinitionLocator definitionLocator(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties properties) {
-
-
         return new DiscoveryClientRouteDefinitionLocator(rdc, properties);
     }
 }
